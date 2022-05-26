9e0a403fa1b1c2bea523bb776fa8ea1422a4bbaa
Gaseema <gaseema.n@gmail.com>
2022-05-26 05:13:02 +0300

    profile update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var nodemailer = require('nodemailer');
-var gMail = require('./GmailPassword')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,7 +38,7 @@ app.post('/mail', (req, res, next) => {
             service: 'Gmail',
             auth: {
                 user: 'gaseema.n@gmail.com',
-                pass: gMail.password
+                pass: process.env.GmailPASS
             }
         });
 
@@ -64,12 +65,12 @@ app.post('/mail', (req, res, next) => {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
